a0dd6c84565ad6b3c9a2c3a2f4ec15dc3ba4e9f2
Francisco Mir <esrd1@Franciscos-MBP.home>
2020-08-12 17:52:36 +0200

    Working on Test Bench view

--- src/components/modalWnd/ModalWnd.js
+++ src/components/modalWnd/ModalWnd.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import {
   CButton,
@@ -10,74 +10,242 @@ import {
   CModalBody,
   CModalFooter,
   CModalHeader,
-  CModalTitle,
   CFormGroup,
   CLabel,
-  CInput
+  CInput,
+  CTextarea,
+  CInputCheckbox,
+  CBadge,
 } from "@coreui/react";
 
 const ModalWnd = (props) => {
-  console.log("HOLA", props);
-  console.log("DATA", props.data, typeof props.data);
+  /** Handle Edition Mode  */
+  const [editMode, setEditMode] = useState(() => {
+    if (props.mode == "edit") return false;
+    return true;
+  });
+  /** Edit Name */
+  const [old_name, setOldName] = useState(props.data.name);
+  const [name, setName] = useState(props.data.name);
+  /** Edit Description */
+  const [old_description, setOldDescription] = useState(props.data.description);
+  const [description, setDescription] = useState(props.data.description);
+  /** Edit Location */
+  const [old_location, setOldLocation] = useState(props.data.location);
+  const [location, setLocation] = useState(props.data.location);
+  /** Edit Devices */
+  const [deviceIDs, setDeviceIDs] = useState(props.data.devices);
+  const [deviceObjects, setDeviceObjects] = useState(() => {
+    if (props.mode == "edit") {
+      console.log("EDIT");
+      let tempArray = [];
+      let tempObject = {};
+      deviceIDs.forEach((deviceID, i) => {
+        tempObject = {
+          id: i,
+          checked: true,
+          name: "Device " + deviceID,
+        };
+        tempArray.push(tempObject);
+      });
+      return tempArray;
+    }
+  });
+  const [old_deviceObjects, setOldDeviceObjects] = useState(deviceObjects);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    console.log("Devices", deviceObjects);
+  }, [deviceObjects]);
+
+  /** Handler for CInputs */
+  function handleOnChange_Name(e) {
+    e.preventDefault();
+    setName(e.target.value);
+  }
+  function handleOnChange_Description(e) {
+    e.preventDefault();
+    setDescription(e.target.value);
+  }
+  function handleOnChange_Location(e) {
+    e.preventDefault();
+    setLocation(e.target.value);
+  }
+
+  /** Handler for CCheckBox */
+  function handleOnChange_Device(e) {
+    //e.preventDefault();
+    console.log(e.target);
+    const deviceID = e.target.id;
+
+    // Update array of Device Objects
+    setDeviceObjects(
+      deviceObjects.map((object) => {
+        if (object.id == deviceID) {
+          // Device to update
+          return { ...object, checked: e.target.checked };
+        }
+        // Device is not the one clicked ==> nothing to do
+        return object;
+      })
+    );
+  }
+
+  /** Handlers for CButtons */
+  function handleClick_Save(e) {
+    e.preventDefault();
+
+    // Update old data with the new changed
+    setOldName(name);
+    setOldDescription(description);
+    setOldLocation(location);
+    setOldDeviceObjects(deviceObjects);
+
+    // Disable Edit mode
+    setEditMode(false);
+  }
+
+  function handleClick_Cancel(e) {
+    e.preventDefault();
+
+    // Discard changed data and keep the old one
+    setName(old_name);
+    setDescription(old_description);
+    setLocation(old_location);
+    setDeviceObjects(old_deviceObjects);
+
+    // Disable Edit mode
+    setEditMode(false);
+  }
+
   return (
-    <CModal show={props.show} onClose={props.onClose}>
+    <CModal show={true} onClose={props.onClose}>
       <CModalHeader closeButton>
-        <CModalTitle>Modal title</CModalTitle>
+        <h3>
+          {props.mode === "edit"
+            ? "Test Bench Information"
+            : "Introducing a new Test Bench"}
+        </h3>
       </CModalHeader>
       <CModalBody>
-        {props.data.id}
-        {props.data.name}
-        {props.data.role}
-
-        <CCol xs="12" sm="6">
+        <CCol>
           <CCard>
-            <CCardHeader>
-              Company
-              <small> Form</small>
-            </CCardHeader>
+            {props.mode === "edit" ? (
+              <CCardHeader>
+                ID: {props.data.id}
+                <CBadge className="float-right" color={props.color}>
+                  {props.data.status}
+                </CBadge>
+              </CCardHeader>
+            ) : (
+              ""
+            )}
             <CCardBody>
               <CFormGroup>
-                <CLabel htmlFor="company">Company</CLabel>
-                <CInput id="company" placeholder="Enter your company name" />
+                <CLabel htmlFor="testbench-name">Test Bench Name</CLabel>
+                <CInput
+                  id="testbench-name"
+                  placeholder="Enter the name of the test bench"
+                  value={name}
+                  disabled={!editMode}
+                  onChange={handleOnChange_Name}
+                  autoFocus
+                />
               </CFormGroup>
               <CFormGroup>
-                <CLabel htmlFor="vat">VAT</CLabel>
-                <CInput id="vat" placeholder="DE1234567890" />
+                <CLabel htmlFor="testbench-description">
+                  Test Bench Description
+                </CLabel>
+                <CTextarea
+                  name="testbench-description-input"
+                  id="testbench-description"
+                  rows="5"
+                  value={description}
+                  placeholder="Describe the test bench..."
+                  disabled={!editMode}
+                  onChange={handleOnChange_Description}
+                />
               </CFormGroup>
               <CFormGroup>
-                <CLabel htmlFor="street">Street</CLabel>
-                <CInput id="street" placeholder="Enter street name" />
+                <CLabel htmlFor="testbench-location">Location</CLabel>
+                <CInput
+                  id="testbench-location"
+                  value={location}
+                  placeholder="Enter location of the test bench"
+                  disabled={!editMode}
+                  onChange={handleOnChange_Location}
+                />
               </CFormGroup>
-              <CFormGroup row className="my-0">
-                <CCol xs="8">
-                  <CFormGroup>
-                    <CLabel htmlFor="city">City</CLabel>
-                    <CInput id="city" placeholder="Enter your city" />
-                  </CFormGroup>
-                </CCol>
-                <CCol xs="4">
-                  <CFormGroup>
-                    <CLabel htmlFor="postal-code">Postal Code</CLabel>
-                    <CInput id="postal-code" placeholder="Postal Code" />
-                  </CFormGroup>
-                </CCol>
-              </CFormGroup>
-              <CFormGroup>
-                <CLabel htmlFor="country">Country</CLabel>
-                <CInput id="country" placeholder="Country name" />
-              </CFormGroup>
+              {props.mode === "edit" ? (
+                <CCard>
+                  <CCardBody>
+                    <CFormGroup row>
+                      <CCol md="9">
+                        <CLabel>Associated Devices</CLabel>
+                        {deviceObjects.map((deviceObject, i) => {
+                          return (
+                            <CFormGroup
+                              key={i}
+                              variant="checkbox"
+                              className="checkbox"
+                            >
+                              <CInputCheckbox
+                                id={i}
+                                name={deviceObject.name}
+                                value={i}
+                                checked={deviceObject.checked}
+                                disabled={!editMode}
+                                onClick={handleOnChange_Device}
+                              />
+                              <CLabel
+                                variant="checkbox"
+                                className="form-check-label"
+                                htmlFor={deviceObject.name}
+                              >
+                                {deviceObject.name}
+                              </CLabel>
+                            </CFormGroup>
+                          );
+                        })}
+                      </CCol>
+                    </CFormGroup>
+                  </CCardBody>
+                </CCard>
+              ) : (
+                ""
+              )}
             </CCardBody>
           </CCard>
         </CCol>
       </CModalBody>
       <CModalFooter>
-        <CButton color="primary">Do Something</CButton>{" "}
-        <CButton color="secondary" onClick={() => props.onClose(false)}>
-          Cancel
-        </CButton>
+        {editMode === false ? (
+          <>
+            <CButton color="danger" onClick={() => setEditMode(true)}>
+              Edit
+            </CButton>{" "}
+            <CButton color="success" onClick={() => props.onClose(false)}>
+              Close
+            </CButton>
+          </>
+        ) : (
+          <>
+            <CButton color="info" onClick={handleClick_Save}>
+              Save
+            </CButton>
+            <CButton color="light" onClick={handleClick_Cancel}>
+              Cancel
+            </CButton>
+          </>
+        )}
       </CModalFooter>
     </CModal>
   );
 };
 
 export default ModalWnd;
+
+/* Los botens
+fetch de los device data para cruzarlo con los IDs
+Actualizar base de datos cuando se da OK
+*/
--- src/views/testbench/Testbench.js
+++ src/views/testbench/Testbench.js
@@ -20,35 +20,58 @@ import {
 import ModalWnd from "../../components/modalWnd/ModalWnd";
 
 /** Update table data */
-import usersData from "../users/UsersData";
+import testBenchesData from "../../assets/myMockups/TestbenchesData";
 
 /** Define table fields */
-const fields = ["name", "registered", "role", "status"];
-
-const getBadge = (status) => {
-  switch (status) {
-    case "Active":
-      return "success";
-    case "Inactive":
-      return "secondary";
-    case "Pending":
-      return "warning";
-    case "Banned":
-      return "danger";
-    default:
-      return "primary";
-  }
-};
+const fields = ["id", "name", "description", "location", "status"];
 
 const Testbench = () => {
   const [visible, setVisible] = React.useState(10);
   const [modal, setModal] = useState(false);
-  const [testbench_data, setTestbenchData]  =useState({id: "", name: "", role: ""})
+  const [testbench_data, setTestbenchData] = useState({
+    id: "",
+    name: "",
+    description: "",
+    location: "",
+    status: "",
+  });
+  const [testbench_color, setTestbenchColor] = useState();
+  const [modalMode, setModalMode] = useState("add");
 
+  const getBadge = (status) => {
+    switch (status) {
+      case "Active":
+        return "success";
+      case "Inactive":
+        return "secondary";
+      case "Pending":
+        return "warning";
+      case "Banned":
+        return "danger";
+      default:
+        return "primary";
+    }
+  };
+
+  function handleOnClick_AddTestBench(e) {
+    e.preventDefault();
+    console.log("ADD", e);
+    setTestbenchData({
+      id: 100,
+      name: "",
+      description: "",
+      location: "",
+      status: "",
+    });
+    setModalMode("add");
+    setModal(true);
+  }
   const handle_onRowClick = (e) => {
     console.log(e);
-    setModal(true)
-    setTestbenchData(e)
+    setTestbenchData(e);
+    setTestbenchColor(getBadge(e.status));
+    setModalMode("edit")
+    setModal(true);
   };
 
   return (
@@ -56,10 +79,19 @@ const Testbench = () => {
       <CRow>
         <CCol>
           <CCard>
-            <CCardHeader>Combined All Table</CCardHeader>
+            <CCardHeader className="tb-list-table-header">
+              <h1 size="sm">List of Test Benches</h1>
+              <CButton
+                className="float-right tb-add-button"
+                color="success"
+                onClick={handleOnClick_AddTestBench}
+              >
+                + Add
+              </CButton>
+            </CCardHeader>
             <CCardBody>
               <CDataTable
-                items={usersData}
+                items={testBenchesData}
                 fields={fields}
                 hover
                 striped
@@ -79,167 +111,17 @@ const Testbench = () => {
                 }}
                 onRowClick={handle_onRowClick}
               />
-            </CCardBody>
 
-            <ModalWnd show={modal} onClose={setModal} data={testbench_data}/>
-            
-          </CCard>
-        </CCol>
-      </CRow>
-      <CRow>
-        <CCol xs="12" md="6">
-          <CCard>
-            <CCardHeader>
-              Alerts
-              <div className="card-header-actions">
-                <a
-                  href="https://coreui.github.io/components/alerts/"
-                  rel="noreferrer noopener"
-                  target="_blank"
-                  className="card-header-action"
-                >
-                  <small className="text-muted">docs</small>
-                </a>
-              </div>
-            </CCardHeader>
-            <CCardBody>
-              <CAlert color="primary">
-                This is a primary alert — check it out!
-              </CAlert>
-              <CAlert color="secondary">
-                This is a secondary alert — check it out!
-              </CAlert>
-              <CAlert color="success">
-                This is a success alert — check it out!
-              </CAlert>
-              <CAlert color="danger">
-                This is a danger alert — check it out!
-              </CAlert>
-              <CAlert color="warning">
-                This is a warning alert — check it out!
-              </CAlert>
-              <CAlert color="info">This is a info alert — check it out!</CAlert>
-              <CAlert color="light">
-                This is a light alert — check it out!
-              </CAlert>
-              <CAlert color="dark">This is a dark alert — check it out!</CAlert>
-            </CCardBody>
-          </CCard>
-        </CCol>
-        <CCol xs="12" md="6">
-          <CCard>
-            <CCardHeader>
-              Alerts
-              <small>
-                {" "}
-                use <code>.alert-link</code> to provide links
-              </small>
-            </CCardHeader>
-            <CCardBody>
-              <CAlert color="primary">
-                {/*eslint-disable-next-line*/}
-                This is a primary alert with&nbsp;
-                <CLink className="alert-link">an example link</CLink>. Give it a
-                click if you like.
-              </CAlert>
-              <CAlert color="secondary">
-                {/*eslint-disable-next-line*/}
-                This is a secondary alert with&nbsp;
-                <CLink className="alert-link">an example link</CLink>. Give it a
-                click if you like.
-              </CAlert>
-              <CAlert color="success">
-                {/*eslint-disable-next-line*/}
-                This is a success alert with&nbsp;
-                <CLink className="alert-link">an example link</CLink>. Give it a
-                click if you like.
-              </CAlert>
-              <CAlert color="danger">
-                {/*eslint-disable-next-line*/}
-                This is a danger alert with&nbsp;
-                <CLink className="alert-link">an example link</CLink>. Give it a
-                click if you like.
-              </CAlert>
-              <CAlert color="warning">
-                {/*eslint-disable-next-line*/}
-                This is a warning alert with&nbsp;
-                <CLink className="alert-link">an example link</CLink>. Give it a
-                click if you like.
-              </CAlert>
-              <CAlert color="info">
-                {/*eslint-disable-next-line*/}
-                This is a info alert with&nbsp;
-                <CLink className="alert-link">an example link</CLink>. Give it a
-                click if you like.
-              </CAlert>
-              <CAlert color="light">
-                {/*eslint-disable-next-line*/}
-                This is a light alert with&nbsp;
-                <CLink className="alert-link">an example link</CLink>. Give it a
-                click if you like.
-              </CAlert>
-              <CAlert color="dark">
-                {/*eslint-disable-next-line*/}
-                This is a dark alert with&nbsp;
-                <CLink className="alert-link">an example link</CLink>. Give it a
-                click if you like.
-              </CAlert>
-            </CCardBody>
-          </CCard>
-        </CCol>
-      </CRow>
-      <CRow>
-        <CCol xs="12" md="6">
-          <CCard>
-            <CCardHeader>
-              Alerts
-              <small> additional content</small>
-            </CCardHeader>
-            <CCardBody>
-              <CAlert color="success">
-                <h4 className="alert-heading">Well done!</h4>
-                <p>
-                  Aww yeah, you successfully read this important alert message.
-                  This example text is going to run a bit longer so that you can
-                  see how spacing within an alert works with this kind of
-                  content.
-                </p>
-                <hr />
-                <p className="mb-0">
-                  Whenever you need to, be sure to use margin utilities to keep
-                  things nice and tidy.
-                </p>
-              </CAlert>
-            </CCardBody>
-          </CCard>
-        </CCol>
-        <CCol xs="12" md="6">
-          <CCard>
-            <CCardHeader>
-              Alerts
-              <small> dismissing</small>
-            </CCardHeader>
-            <CCardBody>
-              <CAlert color="info" closeButton>
-                I am an dismissible alert!
-              </CAlert>
-              <CAlert
-                color="warning"
-                show={visible}
-                closeButton
-                onShowChange={setVisible}
-              >
-                I will be closed in {visible} seconds!
-                <CProgress
-                  striped
-                  color="warning"
-                  value={Number(visible) * 10}
-                  size="xs"
-                  className="mb-3"
+              {modal === true ? (
+                <ModalWnd
+                  onClose={setModal}
+                  data={testbench_data}
+                  color={testbench_color}
+                  mode={modalMode}
                 />
-              </CAlert>
-
-              <CButton onClick={() => setVisible(10)}>Reset timer</CButton>
+              ) : (
+                ""
+              )}
             </CCardBody>
           </CCard>
         </CCol>
